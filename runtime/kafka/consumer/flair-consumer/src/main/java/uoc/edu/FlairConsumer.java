5e5b541b3419c2e9a73be67cc363362a18cfa635
Carles Arnal <carlesarnal92@gmail.com>
2025-03-12 11:06:01 +0100

    Fix quarkus app path and add lsa and vectorizer pickle files

--- runtime/kafka/consumer/flair-consumer/src/main/java/uoc/edu/FlairConsumer.java
+++ runtime/kafka/consumer/flair-consumer/src/main/java/uoc/edu/FlairConsumer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:956d0c2f0ca3c0e2b4182c5cbda737cdf1e415dcd5f18af13828eeb896ea6300
-size 1584
+oid sha256:c0c66f3f59e240ddfb4adc6c5325b2fd5111b4943c22b14293c5da381c8f2f59
+size 1850
--- runtime/spark/reddit_flair_spark_inference.py
+++ runtime/spark/reddit_flair_spark_inference.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:585daa14ef899ab3725ff4c61aa5954641faae062ac000d5cdf18ed9c29a27fe
-size 1839
+oid sha256:b992802a524a4eb9063c28434bc667eeabef261e24807755ca5a9409d94bfac2
+size 2148
