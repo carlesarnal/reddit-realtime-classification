bd6bdb418eabd9f2cd88d259b50d48562b5f166b
Carles Arnal <carlesarnal92@gmail.com>
2025-03-19 11:07:20 +0100

    Add todo

--- runtime/spark/reddit_flair_spark_inference.py
+++ runtime/spark/reddit_flair_spark_inference.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b992802a524a4eb9063c28434bc667eeabef261e24807755ca5a9409d94bfac2
-size 2148
+oid sha256:61c6547dd3667d7c9e3d67429f2dbdfda58e852ce1e9ced06112bdca37bbac94
+size 2206
